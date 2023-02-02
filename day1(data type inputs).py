21831539cb9da6a32d7f6dda265513232ba543be
Lkvijayakumar-python <124118239+Lkvijayakumar-python@users.noreply.github.com>
2023-02-01 22:54:17 -0500

    Add files via upload

---
+++ day1(data type inputs).py
@@ -0,0 +1,21 @@
+
+
+integer_1=int(input("enter first integer value:"))
+interger_2=int(input("enter second integer value:"))
+integer_3=int(input("enter third integer value:"))
+
+
+
+
+float_1=float(input("enter first float value:"))
+float_2=float(input("enter second float value:"))              
+float_3=float(input("enter third float value:"))
+
+              
+string_1=input("enter first string value:")
+string_2=input("enter second string value:")
+string_3=input("enter third string value:")
+
+              
+complex_1=complex(input("enter first complex value:"))
+
